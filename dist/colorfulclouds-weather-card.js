dc521ee39ba9ec5dda1119f3125274b49bd743f3
WalterDSU <91654066+WalterDSU@users.noreply.github.com>
2023-10-08 17:05:22 +0800

    Update colorfulclouds-weather-card.js

--- dist/colorfulclouds-weather-card.js
+++ dist/colorfulclouds-weather-card.js
@@ -1,4 +1,4 @@
-console.info("%c  WEATHER CARD  \n%c  Version 2023.9.22 ",
+console.info("%c  WEATHER CARD  \n%c  Version 2023.10.8 ",
 "color: orange; font-weight: bold; background: black", 
 "color: white; font-weight: bold; background: dimgray");
 
